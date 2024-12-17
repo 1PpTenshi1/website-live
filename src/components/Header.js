ee085db61360175e6d1b32c8ad0dadaacab65d92
1PpTenshi1 <kiritokasuto101@gmail.com>
2024-12-17 21:29:21 +0800

    fnl

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ const Header = () => {
     return (
       <header>
         <nav>
+          <div className="brand">
           <h1>Jadoo</h1>
+          </div>
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/destinations">Destinations</a></li>

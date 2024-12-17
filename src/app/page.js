68e631420baf5f0a57fefadbf583e2515716ec14
1PpTenshi1 <kiritokasuto101@gmail.com>
2024-12-17 18:58:39 +0800

    imgs

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
               <button className="btn">Find out more</button>
             </div>
             <div className="hero-image">
-              <img src= "/src/images/Hero.png" alt="Traveler" />
+              <img src= "/images/Hero.png" alt="Traveler" />
             </div>
           </div>
         </section>
@@ -31,17 +31,17 @@ export default function Home() {
           <h2>Top Destinations</h2>
           <div className="destination-list">
             <div className="destination-card">
-              <img src="/src/images/Boracay.png" alt="Boracay" />
+              <img src="/images/Boracay.png" alt="Boracay" />
               <h3>Boracay, Aklan</h3>
               <p>5 Days Trip</p>
             </div>
             <div className="destination-card">
-              <img src="/src/images/Palawan.png" alt="Palawan" />
+              <img src="/images/Palawan.png" alt="Palawan" />
               <h3>Coron, Palawan</h3>
               <p>4 Days Trip</p>
             </div>
             <div className="destination-card">
-              <img src="/src/images/ChinaTown.png" alt="Manila" />
+              <img src="/images/ChinaTown.png" alt="Manila" />
               <h3>Chinatown, Manila</h3>
               <p>3 Days Trip</p>
             </div>
@@ -53,17 +53,17 @@ export default function Home() {
           <h2>Food Recommendation</h2>
           <div className="food-list">
             <div className="food-card">
-              <img src="/src/images/Chicharon.png" alt="Chicharon" />
+              <img src="/images/Chicharon.png" alt="Chicharon" />
               <h3>Chicharon</h3>
               <p>Best pork belly</p>
             </div>
             <div className="food-card">
-              <img src="/src/images/bananaq.png" alt="Banana Cue" />
+              <img src="/images/bananaq.png" alt="Banana Cue" />
               <h3>Banana Cue</h3>
               <p>Caramelized banana skewers</p>
             </div>
             <div className="food-card">
-              <img src="/src/images/daing.png" alt="Adobo" />
+              <img src="s/images/daing.png" alt="Adobo" />
               <h3>Adobo</h3>
               <p>Dried Fish</p>
             </div>

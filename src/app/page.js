3c2d4cfbf3625bf8024b4c1696d873cd930a93e3
1PpTenshi1 <kiritokasuto101@gmail.com>
2024-12-17 23:01:38 +0800

    fwsafwsa

--- src/app/page.js
+++ src/app/page.js
@@ -11,15 +11,11 @@ export default function Home() {
             <div className="hero-text">
               <h2>BEST DESTINATIONS AROUND THE PHILIPPINES</h2>
               <h1>
-                Travel, enjoy  
+                Travel, enjoy <br></br>and discover <br></br>Philippines!
               </h1>
-              <h1>and discover</h1>
-              <h1>Philippines!</h1>
               <p>
-                Discover the beauty of the Philippines with us, your ultimate travel partner  
+                Discover the beauty of the Philippines with us, your ultimate travel partner <br></br>for unforgettable adventures! We will make it easy for you to <br></br>experience authentic Filipino culture.
               </p>
-              <p>for unforgettable adventures! We will make it easy for you to </p>
-              <p>experience authentic Filipino culture.</p>
               <button className="btn">Find out more</button>
             </div>
             <div className="hero-image">

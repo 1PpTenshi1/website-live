346857808231894359d15cd557ab993739c321ab
1PpTenshi1 <kiritokasuto101@gmail.com>
2024-12-18 15:55:40 +0800

    sda

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
         <nav>
           <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="/destinations">Destinations</a></li>
+            <li><a href="app/destinations">Destinations</a></li>
             <li><a href="/hotels">Foods</a></li>
-            <li><a href="/about.js">About</a></li>
+            <li><a href="/app/about.js">About</a></li>
           </ul>
         </nav>
       </header>

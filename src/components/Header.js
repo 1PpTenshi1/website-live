45fdd0d9d3f423d6fd18a30acbb30ee3d2190e2d
1PpTenshi1 <kiritokasuto101@gmail.com>
2024-12-18 16:05:04 +0800

    as

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
             <li><a href="/">Home</a></li>
             <li><a href="app/destinations">Destinations</a></li>
             <li><a href="/hotels">Foods</a></li>
-            <li><a href="/app/about.js">About</a></li>
+            <li><a href="/about">About</a></li>
           </ul>
         </nav>
       </header>
